0150a4b6965f82290c6589053bce0a4097c38a0d
angelicapedroso <angelicapedroso05@gmail.com>
2022-05-09 19:15:56 -0300

    feat: add funcionalidade de envio de email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import { prisma } from './prisma';
 import express from 'express';
+import nodemailer from 'nodemailer';
 
 const app = express();
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "bc9a83a944d8a3",
+    pass: "f1bfbceefbdd8b"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedback Widget <oi@feedbackwidget.com>',
+    to: 'Angelica Pedroso <angelicapedroso05@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('/n')
   })
 
   return res.status(201).json({ data: feedback });
